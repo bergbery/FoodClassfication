da9fbbed6737c4c5f122cff2ec17b57c51714938
SCNEO <110510687+bergbery@users.noreply.github.com>
2022-08-29 02:47:30 +0800

    Update predict.js

--- static/js/predict.js
+++ static/js/predict.js
@@ -53,13 +53,27 @@ function searchAndDisplayWiki(_vpredictresult) {
 function displayWikiContent(_firstresult) {
 	$.ajax({
 		type: "GET",
-		url: "https://en.wikipedia.org/w/api.php?action=parse&format=json&prop=text&section=0&page=" + _firstresult + "&callback=?",
+		//url: "https://en.wikipedia.org/w/api.php?action=parse&format=json&prop=text&section=0&page=" + _firstresult + "&callback=?&",
+		url: " http://en.wikipedia.org/w/api.php?action=query&prop=extracts&format=json&exintro=&titles=" + _firstresult + "&callback=?&",
 		contentType: "application/json; charset=utf-8",
 		async: false,
 		dataType: "json",
 		success: function (data, textStatus, jqXHR) {
 
-			var markup = data.parse.text["*"];
+			var vpages = data.query.pages;
+			
+			$.each(vpages, function (ind, obj) {
+				var veachpageextract = obj.extract;
+				$('#wikidescription').html(veachpageextract)
+			});
+			/*
+			for (var veachpage in vpages) {
+				var veachpageinfo = veachpage;
+			}
+			//var vpage = eval('data.query.pages.' + vpageid + '.extract');
+			//var markup = data.parse.text["*"];
+			console.log(markup);
+			var num_revisions = data.query.pages[pageId].revisions.length;
 			var blurb = $('<div></div>').html(markup);
 
 			// remove links as they will not work
@@ -82,7 +96,7 @@ function displayWikiContent(_firstresult) {
 			//@blurb.find('.mw-ext-cite-error').remove();
 			$('#wikidescription').html($(blurb).find('p'));
 			$('#wikidescription').html(blurb);
-
+			*/
 		},
 		error: function (errorMessage) {
 			alert(errorMessage);
