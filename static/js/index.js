cab510d994a89efcaf313e48326b3cb6f4975ca9
SCNEO <110510687+bergbery@users.noreply.github.com>
2022-08-31 15:12:56 +0800

    20220831_1500

--- static/js/index.js
+++ static/js/index.js
@@ -1,3 +1,12 @@
 function onchange_submitfile(_this) {
 	$('#submituploadfile').trigger('click');
 }
+
+function onclick_itemreceipe(_this) {
+	if ($(_this).hasClass('nasilemak') == true)) {
+		
+	}
+	else if ($(_this).hasClass('charkwayteow') == true)) {
+		
+	}
+}
